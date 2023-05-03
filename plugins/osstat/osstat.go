753ada29ed62f9aa9ceb389180dc5a2193df96c6
Nelson Lai <nelsonlai9503@gmail.com>
2023-05-03 12:26:00 +0800

    set osstat env

--- plugins/osstat/osstat.go
+++ plugins/osstat/osstat.go
@@ -12,32 +12,36 @@ import (
 
 // MonitorMemory monitor memory usage
 // alertPercent is >= 0 and <= 1
-func MonitorMemory(e *micro.Engine, alertPercent float64, alertFunc func()) {
+func MonitorMemory(e *micro.Engine, alertFunc func()) {
 	micro.Cron(e, "@every 10s", func() {
 		m, err := memory.Get()
 		if err != nil {
 			log.Println("failed to get os memory info", err)
 			return
 		}
-		if float64(m.Used)/float64(m.Total) > alertPercent {
+		if float64(m.Used)/float64(m.Total) > ALERT_PERCENTAGE_MEMORY {
 			logger.Error(auth.GetSystemID(), "", "", "memory used percent alert: ", float64(m.Used)/float64(m.Total))
-			alertFunc()
+			if !DISABLE_ALERT_MEMORY {
+				alertFunc()
+			}
 		}
 	})
 }
 
 // MonitorCPU monitor cpu usage
 // alertPercent is >= 0 and <= 1
-func MonitorCPU(e *micro.Engine, alertPercent float64, alertFunc func()) {
+func MonitorCPU(e *micro.Engine, alertFunc func()) {
 	micro.Cron(e, "@every 10s", func() {
 		c, err := cpu.Get()
 		if err != nil {
 			log.Println("failed to get os cpu info", err)
 			return
 		}
-		if float64(c.User+c.System)/float64(c.Total) > alertPercent {
+		if float64(c.User+c.System)/float64(c.Total) > ALERT_PERCENTAGE_CPU {
 			logger.Error(auth.GetSystemID(), "", "", "cpu used percent alert: ", float64(c.User+c.System)/float64(c.Total))
-			alertFunc()
+			if !DISABLE_ALERT_CPU {
+				alertFunc()
+			}
 		}
 	})
 }
